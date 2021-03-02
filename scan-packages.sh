e7bc80888c868b3ef2b96f3be733831ac6b25927
Kanji <39612139+Slanderrr@users.noreply.github.com>
2021-03-02 14:54:45 -0700

    updates

--- scan-packages.sh
+++ scan-packages.sh
@@ -7,8 +7,3 @@ dpkg-scanpackages -m ./debs > Packages
 echo bzip compressing
 bzip2 -5fkv Packages > Packages.bz2
 
-echo xz compressing
-xz -5fkev Packages > Packages.xz
-
-echo lzma compressing
-xz -5fkev --format=lzma Packages > Packages.lzma
